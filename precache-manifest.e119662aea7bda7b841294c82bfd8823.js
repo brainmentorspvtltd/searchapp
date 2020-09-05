e14afd7a315ed9c72f95ca3d133454a2e5e7f257
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2020-09-05 15:00:36 +0530

    Add files via upload

---
+++ precache-manifest.e119662aea7bda7b841294c82bfd8823.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d549dce190bb076c121067aa7ead748",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4391616ce3f69f5845f7",
+    "url": "/static/js/2.43b9fa22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.43b9fa22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec3fecff62e8d0f29d1a",
+    "url": "/static/js/main.9d17de7f.chunk.js"
+  },
+  {
+    "revision": "ffe148ba9c987666fc58",
+    "url": "/static/js/runtime-main.451dc41f.js"
+  }
+]);
